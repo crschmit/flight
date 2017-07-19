b8be0b710f6698ca2a47430de9f29f621b09bb77
unknown <crschmit@gmail.com>
2017-07-19 02:17:31 -0500

    added origin buttons to flights tab

--- ui/src/app/flights/flights.component.js
+++ ui/src/app/flights/flights.component.js
@@ -2,20 +2,30 @@ import templateUrl from './flights.component.html'
 
 /* @ngInject */
 const controller = class FlightsController {
-  constructor ($log, $flights) {
+  constructor ($log, $flights, $locations) {
     $log.log('FlightsController ...')
 
     this.$flights = $flights
+    this.$locations = $locations
 
     this.flights = []
     $flights.getAllFlights()
       .then(res => {
         this.flights = res
       })
+
+    this.locations = []
+    $locations.getAllLocations()
+      .then(res => {
+        this.locations = res})
   }
 
   getAllFlights () {
     return this.flights
+  }
+
+  getAllLocations () {
+    return this.locations
   }
 }
 
