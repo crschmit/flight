7db8e6ddbc7b1b80cd86c3478bd2ede58ddacd75
unknown <crschmit@gmail.com>
2017-07-19 13:35:59 -0500

    added origin filter to flights.service

--- ui/src/app/flights/flights.service.js
+++ ui/src/app/flights/flights.service.js
@@ -11,14 +11,22 @@ class FlightsService {
 
     this.guard = this.Flight.guard
     this.matchAny = this.Flight.match('Any') // Any -> Boolean
+    this.anyOrigin = orig => f => this.matchAny(orig) // Flight -> Boolean
     this.keepAny = this.Flight.always(true) // Flight -> Boolean
-    this.keepFrom = this.Flight.from // City -> Flight -> Boolean
+    this.keepFrom = orig => this.Flight.from(orig) // Flight -> Boolean
     // this.originFilter = orig =>
     //   this.guard(
     //     this.matchAny,
     //     this.keepAny,
     //     this.keepFrom(orig)
     //   )(orig)
+    // City + Any -> Flight -> Boolean
+    this.originFilter = orig =>
+      this.guard(
+        this.anyOrigin(orig),
+        this.keepAny,
+        this.keepFrom(orig)
+      )
   }
 
   getAllFlights () {
@@ -36,28 +44,28 @@ class FlightsService {
   }
 
   getAvailableFlights () {
-    // if (this.origin === 'Any') {
-    if (this.matchAny(this.origin)) {
-      // return this.getAllFlights()
-      return this.$http
-        .get(`${this.apiUrl}/flights`)
-        .then(result => result
-                          .data
-                          .filter(this.keepAny))
-    }
-    else {
-      return this.$http
-        .get(`${this.apiUrl}/flights`)
-        .then(result => result
-                          .data
-                          .filter(this.keepFrom(this.origin)))
-    }
+    // // if (this.origin === 'Any') {
+    // if (this.matchAny(this.origin)) {
+    //   // return this.getAllFlights()
+    //   return this.$http
+    //     .get(`${this.apiUrl}/flights`)
+    //     .then(result => result
+    //                       .data
+    //                       .filter(this.keepAny))
+    // }
+    // else {
+    //   return this.$http
+    //     .get(`${this.apiUrl}/flights`)
+    //     .then(result => result
+    //                       .data
+    //                       .filter(this.keepFrom(this.origin)))
+    // }
 
-    // return this.$http
-    //   .get(`${this.apiUrl}/flights`)
-    //   .then(result => result
-    //                     .data
-    //                     .filter(this.originFilter(this.origin)))
+    return this.$http
+      .get(`${this.apiUrl}/flights`)
+      .then(result => result
+                        .data
+                        .filter(this.originFilter(this.origin)))
   }
 
 }
